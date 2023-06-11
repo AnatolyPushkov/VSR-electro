6e26937b3789287a99fab6d82e19e7c4ba2f539f
Anatoly <Anatoly@mail.com>
2023-06-11 23:57:44 +0300

    first commit

---
+++ src/js/sl.js
@@ -0,0 +1,9 @@
+$('.your-class').slick(
+    {
+
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next_1"><img src="icon/next.png"></button>'
+    }
+
+
+);
